b89f660905ca3424da02c2194285c457a1c4cf13
MadeByGabbe <97948127+MadeByGabbe@users.noreply.github.com>
2022-03-24 17:14:08 +0100

    Getting courses
    Fetching courses works but with several bugs probably placement issue

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ function elementSelector(select) {
     return document.querySelector(select);
 }
 
-//Clear content inside search-results div
+// Clear content inside search-results div
 function clearResults() {
     elementSelector('.search-results').innerHTML = "";
 }
 
-
+// Renders result from search box input
 function getResults() {
     let search = elementSelector('.searchbar').value;
     
@@ -23,16 +23,32 @@ function getResults() {
                 
                 elementSelector('.search-results').innerHTML += `
                 
-                <div class="search-div">
-                    <span class="search-item">${DATABASE.students[i].firstName}</span>
-                    <span class="search-item">${DATABASE.students[i].lastName}</span>
-                </div>
+                    <div class="search-div">
+                        <span class="search-item">${DATABASE.students[i].firstName}</span>
+                        <span class="search-item">${DATABASE.students[i].lastName}</span>
+                        <div class="search-courses"></div>
+                    </div>
                 
                 `
+                for (let studentCourse of DATABASE.students[i].courses) {
+                    for (let dbCourse of DATABASE.courses) {
+                        if (studentCourse.courseId == dbCourse.courseId) {
+                            elementSelector('.search-courses').innerHTML += `
+                            <div class="course-title">${dbCourse.title}</div>
+                            <div class="studentCourse-start">${studentCourse.started.semester} ${studentCourse.started.year}</div>
+                            <div class="course-credits">${studentCourse.passedCredits}/${dbCourse.totalCredits}</div>
+                            
+                            `
+            
+                        }
+                    }
                 }
+            }
         }
-    
+        
     }
+    
 }
+
 
 elementSelector('.searchbar').addEventListener('keyup', getResults);
