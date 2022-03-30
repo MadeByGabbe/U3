c012accb8bf67d064f2d7b58b18009c51256555c
MadeByGabbe <97948127+MadeByGabbe@users.noreply.github.com>
2022-03-30 17:36:57 +0200

    Course Responsible and Teachers are in

--- courses.js
+++ courses.js
@@ -1,56 +1,16 @@
 "use strict" 
 
-// // Choose a element based on what is given in parameter
-// let selectElement = (select) => {
-//     return document.querySelector(select); 
-// }
+// Choose a element based on what is given in parameter
+let selectElement = (select) => {
+    return document.querySelector(select); 
+}
 
-// // Cleanse the content 
-// let clearResults = () => {
-//     selectElement("#search-result").innerHTML = "";
-// }
+// Cleanse the content 
+let clearResults = () => {
+    selectElement("#search-result").innerHTML = "";
+}
 
-// function getTotalCredits (counter) {
-//     let credits = []; 
-
-//     for (let studentCourse of DATABASE.students[counter].courses) {
-//         credits.push(studentCourse.passedCredits);
-//     }
-
-//     let creditSum = 0; 
-
-//     // for (let i = 0; i < credits.length; i++) {
-//     //     creditSum += credits[i]
-//     // }
-
-//     credits.forEach((credit => {
-//       creditSum += credit
-//     }) )
-
-//     return creditSum;
-// }
-
-// function sortStudents () {
-    
-//     DATABASE.students.sort(function(a, b) {
-    
-//     if ( a.firstName > b.firstName ) {
-//       return 1;
-//     }
-
-//     else if ( a.firstName < b.firstName ) {
-//       return -1;
-//     }
-
-//     return 0
-//   });
-  
-// }
-
-//////////////
-
-// let courses = DATABASE.courses;
-
+// sort 
 function sortCourseTitle () {
 
     DATABASE.courses.sort((a, b) => {
@@ -65,9 +25,11 @@ function sortCourseTitle () {
     })
   
   }
-  
+
+
+// Get responsibel for course 
 function responsibelOfCourse (counter) {
-    
+  
     let course = DATABASE.courses[counter]; 
   
     let teachersName = DATABASE.teachers.map((teacher) => `${teacher.firstName} ${teacher.lastName} (${teacher.post})`)
@@ -76,70 +38,46 @@ function responsibelOfCourse (counter) {
   
     return teachersName[result];
   
-  }
-  
-  
+}
+
+
 function teacherInCourse (counter) {
-    
+  
     let course = DATABASE.courses[counter].teachers;
     
-    // console.log(course)
+    console.log(course)
     
     for ( let course of DATABASE.courses[counter].teachers) {
-      // console.log(course)
+      
+        console.log(course)
     
       for ( let teachers of DATABASE.teachers ) {
     
+        console.log(teachers)
+
         if ( course == teachers.teacherId) {
-          
-          selectElement(`.name-teachers`).innerHTML += `
-          <div>
-          ${teachers.firstName} ${teachers.lastName}  ( ${teachers.post} )
-          </div>
+
+          selectElement(`div > div:last-child > .name-teachers`).innerHTML += `
+            <div>
+                ${teachers.firstName} ${teachers.lastName}  (${teachers.post})
+            </div>
           `
-        }   
+
+        }  
       }  
     }
   }
   
+
   function getStudentsFromCourse (counter) {
   
-  
-  }
-  
     
-  //Skapar en dark/light mode funktion där "knappen/button" skapas till en eventListener 
-function darkMode() {
-    // selectElement("body")
-    var element = document.body;
-    const darkMode = localStorage.getItem("darkMode")
-    element.classList.toggle("darkMode");
   
-    if (JSON.parse(darkMode) == true) {
-        element.classList.remove("darkMode");
-        localStorage.setItem("darkMode", JSON.stringify(false));
-    } 
-    else if (JSON.parse(darkMode) == false) {
-        element.classList.add("darkMode");
-        localStorage.setItem("darkMode", JSON.stringify(true));
-    }
+  
   }
-  
-  const btn = document.querySelector('.theme')
-  btn.addEventListener('click', darkMode);
 
 
-// Choose a element based on what is given in parameter
-let selectElement = (select) => {
-    return document.querySelector(select); 
-}
-
-// Cleanse the content 
-let clearResults = () => {
-    selectElement("#search-result").innerHTML = "";
-}
-
-function getTeacersOnSearch () {
+function getTeachersOnSearch () {
 
     let search = selectElement("#searchbar").value;
     // console.log(search);
@@ -167,23 +105,47 @@ function getTeacersOnSearch () {
                         </div>
 
                         <div class="name-teachers"> 
-                            <div class="responsibleTeacher">${responsibelOfCourse(i)}</div>
-                            <div class="teachersInClass"></div>
+                            <div class="responsibleTeacher">
+                                ${responsibelOfCourse(i)}
                             </div>
                             
-                            <p> Students </p>
-                            <div class="students">
-                            ${getStudentsFromCourse(i)}
-                            </div>
+                        </div>
                             
-                            </div>
-                            `
-                            // teacherInCourse(i)
-                            // getStudentsFromCourse(i)
+                        <p> Students </p>
+                        <div class="students">
+                            
+                        </div>
+                            
+                    </div>
+                 `
+                    teacherInCourse(i)
+                    // getStudentsFromCourse(i)
             }
         }
     } 
 }
 
 
-selectElement("#searchbar").addEventListener("keyup", getTeacersOnSearch);
+
+
+//Skapar en dark/light mode funktion där "knappen/button" skapas till en eventListener 
+function darkMode() {
+    
+    var element = document.body;
+    const darkMode = localStorage.getItem("darkMode")
+    element.classList.toggle("darkMode");
+    
+    if (JSON.parse(darkMode) == true) {
+        element.classList.remove("darkMode");
+        localStorage.setItem("darkMode", JSON.stringify(false));
+    } 
+    else if (JSON.parse(darkMode) == false) {
+        element.classList.add("darkMode");
+        localStorage.setItem("darkMode", JSON.stringify(true));
+    }
+    
+}
+
+selectElement(".theme").addEventListener("click", darkMode)
+
+selectElement("#searchbar").addEventListener("keyup", getTeachersOnSearch);
